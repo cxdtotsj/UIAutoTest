40c042b7a07b836c7517bd91f804cb6ddd81d4bd
cxdtotsj <cxdtotsj@163.com>
2019-06-14 17:26:54 +0800

    增加login case test

---
+++ case/test_login.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pages.login import LoginPage
+from base.drivers import Drivers
+import unittest
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        self.driver = Drivers("chrome").driver
+        self.login_page = LoginPage(self.driver)
+        self.login_page.open("/admin/")
+
+    def test_check_login_name(self):
+        login_info = ("xdchenadmin@admin", "12345678")
+        self.assertEqual(self.login_page.login_success_user(*login_info), "xdchenadmin@admin")
