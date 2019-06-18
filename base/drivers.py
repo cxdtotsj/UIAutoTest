5c9be6596cb581367badd25b72779c8c946e066f
cxdtotsj <cxdtotsj@163.com>
2019-06-18 11:09:59 +0800

    option 添加 disable-gpu

--- base/drivers.py
+++ base/drivers.py
@@ -37,16 +37,22 @@ class Drivers:
         if brower =='Chrome' or brower =='chrome' or brower =='Ch' or brower=='ch':
             option = webdriver.ChromeOptions()
             option.add_argument("headless")
+            # 禁用GPU硬件加速，防止出现BUG
+            option.add_argument("disable-gpu")
             driver = webdriver.Chrome(executable_path=CHROME_PATH, chrome_options=option)
             logger.info("启动 Chrome--Headless 浏览器")
         elif brower =='firefox' or brower =='Firefox' or brower =='f' or brower =='F':
             option = webdriver.FirefoxOptions()
             option.add_argument("headless")
+            # 禁用GPU硬件加速，防止出现BUG
+            option.add_argument("disable-gpu")
             driver = webdriver.Firefox(executable_path=FIREFOX_PATH, firefox_options=option)
             logger.info("启动 Firefox-Headless 浏览器")
         elif brower =='Ie' or brower =='ie' or brower =='i' or brower=='I':
             option = webdriver.IeOptions()
             option.add_argument("headless")
+            # 禁用GPU硬件加速，防止出现BUG
+            option.add_argument("disable-gpu")
             driver = webdriver.Ie(ie_options=option)
             logger.info("启动 IE-Headless 浏览器")
         else:
