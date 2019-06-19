53e8d6a0203324c39b9cd27ab472d50811ba7595
cxdtotsj <cxdtotsj@163.com>
2019-06-19 13:54:16 +0800

    测试remote

--- case/test_login.py
+++ case/test_login.py
@@ -3,17 +3,19 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pages.login import LoginPage
-from base.drivers import DriversRemote
+from base.drivers import DriversRemote, Drivers
 import unittest
 
 class TestLogin(unittest.TestCase):
     def setUp(self):
-        self.driver = DriversRemote('chrome').driver
+        # self.driver = DriversRemote('chrome').driver
+        self.driver = Drivers('remote', 'chrome')
         self.login_page = LoginPage(self.driver)
         self.login_page.open("/admin/")
 
     def test_check_login_name(self):
         login_info = ("xdchenadmin@admin", "12345678")
+        print(self.login_page.login_success_user())
         self.assertEqual(self.login_page.login_success_user(*login_info), "xdchenadmin@admin")
     
     def tearDown(self):
